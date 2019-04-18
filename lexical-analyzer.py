155d93aa4cb2b758a2b2902b71e8b54f85ffe36c
Leo PAOL <leo.paol@epitech.eu>
2019-04-18 15:32:49 +0900

    Main file

---
+++ lexical-analyzer.py
@@ -0,0 +1,13 @@
+#
+# Team 1 :  Leo PAOL & Christophe MEI
+#           50181573 & 50181587
+#
+
+import sys
+
+def main():
+    print("hello")
+
+if __name__ == '__main__':
+    # execute only if run as the entry point into the program
+    main()
