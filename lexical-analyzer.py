fb38d2a97dea3e9cc91d6b0f46ad7f755387d52e
Leo PAOL <leo.paol@epitech.eu>
2019-04-18 15:41:55 +0900

    args checking

--- lexical-analyzer.py
+++ lexical-analyzer.py
@@ -5,8 +5,14 @@
 
 import sys
 
+def check_args():
+    if len(sys.argv) != 2:
+        return False
+    return True
+
 def main():
-    print("hello")
+    if check_args() == False:
+        sys.exit("USAGE: python3 %s <file_to_analyze>" % sys.argv[0])
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
