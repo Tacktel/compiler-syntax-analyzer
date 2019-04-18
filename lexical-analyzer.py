602e1cf4f6c4c5ac1e2715bc24e7800f680f19bd
Leo PAOL <leo.paol@epitech.eu>
2019-04-18 15:48:32 +0900

    reading file ok

--- lexical-analyzer.py
+++ lexical-analyzer.py
@@ -10,9 +10,22 @@ def check_args():
         return False
     return True
 
+def get_file_content():
+    try:
+        file = open(sys.argv[1], "r")
+    except IOError:
+        sys.exit("Error while opening file %s" % sys.argv[1])
+    if file.mode == "r":
+        file_content = file.read()
+    else:
+        sys.exit("Error while reading file %s" % sys.argv[1])
+
+    return file_content
+
 def main():
     if check_args() == False:
         sys.exit("USAGE: python3 %s <file_to_analyze>" % sys.argv[0])
+    file_content = get_file_content()
 
 if __name__ == '__main__':
     # execute only if run as the entry point into the program
